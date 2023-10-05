e3483711296d677cee963a5a051a807726b2d731
Daria <jan.kowalski@pollub.edu.pl>
2023-10-05 15:33:10 +0200

    Feat/pollub-2

--- src/User.java
+++ src/User.java
@@ -2,4 +2,13 @@ public class User {
     public void hello() {
         System.out.println("hello");
     }
+    public void hello2(){
+        System.out.println("hello2");
+    }
+    public void hello3(){
+        System.out.println("hello2");
+    }
+    public void hello4(){
+        System.out.println("hello2");
+    }
 }
